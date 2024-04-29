3d6089967ac9b66d7b9de5038302f2424547b749
GABRIEL MATHEUS ALVES DO NASCIMENTO SOUSA <gabriel.mansousa@senacsp.edu.br>
2024-04-29 11:53:23 -0300

    load 01

--- index.js
+++ index.js
@@ -1,42 +1,66 @@
-let a = document.getElementById("meu")
-a.addEventListener("mouseover", botao)
-a.addEventListener("mouseout", saindo)
+// let a = document.getElementById("meu");// valor necessário para o todo
 
-function botao(){
-    a = document.getElementById("meu").innerText = "mudei denovo"
-    a.style.background = "blue"
+// a.addEventListener("click", clicar); //escutando o evento
+// a.addEventListener("mouseout", tirei); //
+
+// function clicar(){
+//     a.innerText="cliquei"
+//     a.style.background="green"
+// }
+// function tirei(){
+//     a.innerText="tiro"
+//     a.style.background="orange"
+// }
+
+
+// let valor = 10; //começo a colocar conta
+
+
+// document.getElementById("letra").innerText= valor;
+
+// function btn1(){
+//     aumentar = valor +=10
+//     document.getElementById("letra").innerText= aumentar;
+
+
+//     let total = 20;//a partir daqui tem if
+
+// if(valor == total%2){
+//     document.getElementById("letra").innerHTML="par"
+// }else{
+//     let body = document.body;
+//     body.style.background = "green"
+// }
+
+// }
+
+// function btn2(){
+//     diminuir = valor -=10
+//     document.getElementById('letra').innerText= diminuir;
+
+//  let total = 20;
+//  if(valor == total){
+//     let body2 = document.body2;
+//     body2.style.background = "red"
+//  }
+    
+// }
+
+
+
+
+
+
+function botao() {
+var txt = document.getElementById("txt");
+var res = document.getElementById("resposta");
+var vel = Number(txt.value);
+
+res.innerHTML = `a velocidade atual é de ${vel} km/h`
+
+if(vel > 200){
+    document.getElementById("resposta").innerHTML +=  "<p>metendo o louco</p>"
+}else{
+    res.innerHTML +=" MACHO"
 }
-
-
-function saindo(){
-a.innerHTML = "saindo e mudando, né?"
-a.style.color = "green"
-}
-
-
-
-
-let valor = 0;
-document.getElementById("letra"),innerText = valor;
-
-function btn1(){
-aumentar = valor +=1;
-document.getElementById("letra").innerText = aumentar
-
-let oposto = 5;
-if(valor == 5 || valor == 10){
-    var body = document.body;
-    body.style.background = "red"
-}
-else{
-    var body2 = document.body;
-    body2.style.background = "green"
-}
-
-
-}
-
-function btn2(){
-    diminuir = valor -=1;
-    document.getElementById("letra").innerText = diminuir
 }
